7c5cb0fcd7d3d36c30663c6b8063d0c5e395ef27
Leonardo Velasco Ojeda <leonardo.velasco.oj@gmail.com>
2024-11-15 15:49:32 -0600

    ajuste a la organización y archivos duplicados

--- app/models.py
+++ app/models.py
@@ -1,2 +1,60 @@
-# Define las clases para las tablas country, city y countrylanguage usando SQLAlchemy. 
-# Establece las relaciones entre ellas mediante claves foráneas.
+import enum
+from unittest.mock import Base
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import Float,Integer,Text,SmallInteger
+
+#DEFINICION DE MODELOS
+#Aqui se define cada tabla de la base de datos como una clase en SQLAlchemy con las relaciones necesarias.
+
+#Lista de Continentes
+class continentList(enum.Enum):
+    'Asia'=1
+    'Europe'=2
+    'North America'=3
+    'Africa'=4
+    'Oceania'=5
+    'Antarctica'=6
+    'South America'=7
+
+#Lista de opciones de idioma oficial
+class isofficialList(enum.Enum):
+    'F'=0
+    'T'=1
+
+#Clase Country con sus relaciones
+class Country(Base):
+    __tablename__ = "country"
+
+    code = Column(Text(length=3), primary_key=True)
+    name = Column(Text(length=52))
+    continent = Column(enum(continentList))
+    region = Column(Text(length=26))
+    surfeceArea = Column(Float)
+    indepYear = Column(SmallInteger)
+    population = Column(Integer)
+    lifeExpectancy = Column(Float)
+    gnp = Column(Float)
+    gnpOld = Column(Float)
+    localName = Column(Text(length=45))
+    governmentForm = Column(Text(length=45))
+    headOfState = Column(Text(length=60))
+    capital = Column(Integer)
+    code2 = Column(Text(length=2))
+
+#Clase City con sus relaciones
+class City(Base):
+    __tablename__ = "city"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Text(length=35))
+    countryCode = Column(Text(length=3),ForeignKey("country.code"))
+    district = Column(Text(length=20))
+    population = Column(Integer)
+
+#Clase CountryLanguage con sus relaciones
+class CountryLanguage(Base):
+    __tablename__ = "countryLanguage"
+
+    countryCode = Column(Text(length=3), primary_key=True)
+    language = Column(Text(length=30), primary_key=True)
+    isOfficial = Column(enum(isofficialList))
--- world_queries/config.py
+++
@@ -1,4 +0,0 @@
-#CONFIGURACIÒN
-#Configuraciòn de la base de datos y de la aplicaciòn
-#Configuraciòn App
-#Configuraciòn Base de datos
