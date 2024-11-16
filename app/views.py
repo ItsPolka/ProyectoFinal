2618e95ff4aaa5362d1069f3635459480fd337d0
Leonardo Velasco Ojeda <leonardo.velasco.oj@gmail.com>
2024-11-16 00:41:02 -0600

    Added new images and updated templates and styles

--- app/views.py
+++ app/views.py
@@ -1,44 +1,12 @@
-#CONTROLADORES
+#VIEWS
 #Este archivo meneja las rutas de Flask, llamando a funciones de consulta y pasando los resultados a templates.
+from flask import Flask, render_template
+from flask import Blueprint
 
+views = Blueprint('views', __name__)
 
-from flask import Flask, render_template, url_for
-#from app.queries import nameOfFunction
-app = Flask(__name__)
-
-@app.route('/')
+    # Define la ruta de inicio
+@views.route('/')  # Decorador para definir la ruta raíz
 def index():
+    # Renderiza el template 'index.html'
     return render_template('index.html')
-
-@app.route('/consulta1')
-def consulta_1():
-    #resultados= nameOfFunction()
-    #return render_template('consulta_1.html',resultados=resultados)
-    return render_template('consulta_1.html')
-
-@app.route('/consulta2')
-def consulta_2():
-    #resultados= nameOfFunction()
-    #return render_template('consulta_2.html',resultados=resultados)
-    return render_template('consulta_2.html')
-
-@app.route('/consulta3')
-def consulta_3():
-    #resultados= nameOfFunction()
-    #return render_template('consulta_3.html',resultados=resultados)
-    return render_template('consulta_3.html')
-
-@app.route('/consulta4')
-def consulta_4():
-    #resultados= nameOfFunction()
-    #return render_template('consulta_4.html',resultados=resultados)
-    return render_template('consulta_4.html')
-
-@app.route('/consulta5')
-def consulta_5():
-    #resultados= nameOfFunction()
-    #return render_template('consulta_5.html',resultados=resultados)
-    return render_template('consulta_5.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
